51fa0babfe62828b8b11b85bb4ce7cdb870e3925
María <xmaryjs99@gmail.com>
2025-05-24 12:02:38 +0200

    faltan hacer las pruebas

---
+++ server.js
@@ -0,0 +1,28 @@
+try {
+    process.loadEnvFile()
+} catch (error) {
+    console.warn("archivo .env no encontrado, usando variables predeterminadas")
+}
+
+const { json } = require("body-parser")
+const jsonServer = require("json-server")
+
+const server = jsonServer.create()
+
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+
+const routes = jsonServer.router("db.json")
+
+server.use(routes)
+
+const PORT = process.env.PORT || 5005
+
+server.listen(PORT, () => {
+    console.log(`JSON server ejecutandose en el puerto ${PORT}`)
+    console.log(`Acceso Local en url http://localhost:${PORT}`)
+})
+
+
+
